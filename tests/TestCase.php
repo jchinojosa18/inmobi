ec279123f8cf4214c937b257d0dd1df08a2d8c5c
JCHinojosa18 <hinojosa.encinas.jose@gmail.com>
2026-03-04 06:53:07 -0800

    test vite fallaba se arreglo

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,29 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Vite as ViteClass;
+use Illuminate\Support\Facades\Vite;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        if (! ViteClass::hasMacro('fake')) {
+            ViteClass::macro('fake', function (?string $devServerUrl = null) {
+                $hotFile = storage_path('framework/testing-vite.hot');
+
+                if (! is_dir(dirname($hotFile))) {
+                    mkdir(dirname($hotFile), 0775, true);
+                }
+
+                file_put_contents($hotFile, rtrim($devServerUrl ?? 'http://127.0.0.1:5173', '/'));
+
+                return $this->useHotFile($hotFile);
+            });
+        }
+
+        Vite::fake();
+    }
 }
